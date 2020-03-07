539bea4ce0b588e9eed768dbec5265c069d5c57c
Chisato Murakami <kiwipom1031@gmail.com>
2020-03-07 08:12:31 -0800

    added trash icon

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const formElement = document.querySelector('#new-todo-form');
 const inputElement = document.querySelector('#new-list-input');
 const newList = document.querySelector('#new-list');
 const newDiv = document.getElementsByClassName('list');
+const newIcon = document.querySelector('fas fa-trash-alt')
 
 const btnClickHandler = (btn, span) => {
     btn.addEventListener('click', () => {
@@ -29,20 +30,25 @@ for (let i = 0; i < tasks.length; i++){
 formElement.addEventListener("submit", (event) => {
     event.preventDefault() //atode
     if(inputElement.value !== ""){
-        console.log(inputElement);
+        // console.log(inputElement);
         let btnElement = document.createElement('button');//新しいbuttonを作った
         let spanElement = document.createElement('span');//新しいspanを作った
         let divElement = document.createElement('div');
+        let deleteIconElement = document.createElement('i');
+
         btnElement.classList.add('btn');//buttonクラス作った
-        btnElement.textContent = 'DKT'; //DKT入れた
+        btnElement.textContent = 'DKT'; //DKTmoji入れた
         spanElement.classList.add('task');//spanクラス作った
         spanElement.textContent = ` ${inputElement.value}`; // inputで入れたvalueを入れた
         divElement.classList.add('list'); // divクラス作った
+        deleteIconElement.classList.add('fas', 'fa-trash-alt'); //iconクラス作った
         formElement.reset(); //formだけに使える、value入れた後にリセットされて消えるように
     
         newList.appendChild(btnElement);//親を作った後でくっつけた(const newList) 
         newList.appendChild(spanElement);
+        newList.appendChild(deleteIconElement);
         newList.appendChild(divElement);
+
         
         // for (let i = 0; i < tasks.length; i++){
             // btnElement.addEventListener('click', () => {
